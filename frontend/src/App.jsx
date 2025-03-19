c5c5d0102dadc32f81732a539ae78a4a1c0e9498
Chaitanya <bhavarrudra42@gmail.com>
2025-03-19 03:34:13 +0000

    tailwind css setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <button class="bg-indigo-500 ...">
+  Save changes
+</button>
+      <p class="text-sky-400">The quick brown fox...</p>
+ 
+    </>
+  )
+}
+
+export default App
